28ffcee96ff3000c77a8da72c2b38c27ad99d309
flayner2 <flayner5@gmail.com>
2021-02-05 16:22:25 -0300

    coded the functionality of the get_seq_class function

--- helpers/masked_seqs_stats.py
+++ helpers/masked_seqs_stats.py
@@ -1,5 +1,4 @@
 from Bio.SeqIO import SeqRecord
-from typing import Union
 import re
 
 
@@ -30,8 +29,34 @@ def find_x_regions_and_calculate_stats(
         seq_features_list.append(seq_features)
         x_group_counter += 1
 
+    if len(seq_features_list) > 0:
+        seq_class = get_seq_class(seq_features_list)
+
+        for each_dict in seq_features_list:
+            each_dict["seq_class"] = seq_class
+
     return seq_features_list
 
 
-def get_seq_class():
-    pass
+def get_seq_class(x_groups_list: list[dict]) -> int:
+    num_of_xgroups = len(x_groups_list)
+
+    if num_of_xgroups == 2:
+        return 1
+    elif num_of_xgroups > 2:
+        return 2
+    else:
+        x_group = x_groups_list[0]
+        xgrop_len = x_group["xgroup_len"]
+        dist_from_5 = x_group["dist_from_5"]
+
+        if xgrop_len <= 300 and dist_from_5 < 50:
+            return 3
+        elif xgrop_len > 300 and dist_from_5 < 50:
+            return 4
+        elif xgrop_len <= 300 and (50 < dist_from_5 <= 300):
+            return 5
+        elif xgrop_len > 300 and (50 < dist_from_5 <= 300):
+            return 6
+        else:
+            return 7
