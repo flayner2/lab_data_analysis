58af30cef836b6070be4ac49cdda344082304453
flayner2 <flayner5@gmail.com>
2021-02-05 14:16:49 -0300

    created the functions to calculate the distance of an x-region to each end

--- helpers/masked_seqs_stats.py
+++ helpers/masked_seqs_stats.py
@@ -1,9 +1,29 @@
-def get_dist_to_3prime():
+from Bio.SeqIO import SeqRecord
+from typing import Union
+
+
+def find_x_regions_and_calculate_stats(
+    sequence: SeqRecord,
+) -> Union[dict[str, Union[str, int]], None]:
     pass
 
 
-def get_dist_to_5prime():
-    pass
+def get_dist_to_3prime(sequence: SeqRecord) -> Union[int, None]:
+    first_x_position = sequence.seq.find("X")
+
+    if first_x_position != -1:
+        return first_x_position
+    else:
+        return None
+
+
+def get_dist_to_5prime(sequence: SeqRecord) -> Union[int, None]:
+    last_x_position = sequence.seq.rfind("X")
+
+    if last_x_position != -1:
+        return len(sequence.seq) - last_x_position - 1
+    else:
+        return None
 
 
 def get_xregion_len():
