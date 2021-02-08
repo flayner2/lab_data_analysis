65d70bc0845d640dca01207bd6c081dd3136d3e8
flayner2 <flayner5@gmail.com>
2021-02-08 10:55:24 -0300

    changed a column name and how xgroups are counted

--- helpers/masked_seqs_stats.py
+++ helpers/masked_seqs_stats.py
@@ -6,7 +6,7 @@ def find_x_regions_and_calculate_stats(
     sequence: SeqRecord, taxon: str
 ) -> list:
     seq_features_list = []
-    x_group_counter = 0
+    x_group_counter = 1
 
     pattern = re.compile(r"X+")
     search_res = pattern.finditer(str(sequence.seq))
@@ -21,7 +21,7 @@ def find_x_regions_and_calculate_stats(
         start, end = match.span()
         xgroup_len = len(match.group())
 
-        seq_features["xgroup_id"] = x_group_counter
+        seq_features["seq_xgroup_count"] = x_group_counter
         seq_features["xgroup_len"] = xgroup_len
         seq_features["dist_from_3"] = start
         seq_features["dist_from_5"] = seq_features["seq_len"] - end
