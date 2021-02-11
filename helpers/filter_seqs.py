7d117f178d90d9f5d9bb529123df66151e42dda4
flayner2 <flayner5@gmail.com>
2021-02-11 15:06:21 -0300

    Prototyped the filtering functions

---
+++ helpers/filter_seqs.py
@@ -0,0 +1,21 @@
+from Bio.Seq import Seq
+
+
+def process_seq_by_class(seq: Seq, seq_class: int) -> Seq:
+    pass
+
+
+def trim_x_only_keep_in_between(seq: Seq) -> Seq:
+    pass
+
+
+def trim_x_only(seq: Seq) -> Seq:
+    pass
+
+
+def trim_x_and_5prime(seq: Seq) -> Seq:
+    pass
+
+
+def trim_x_and_3prime(seq: Seq) -> Seq:
+    pass
