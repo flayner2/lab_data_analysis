5d842ae403a008d3afae29a8dfe915fee1f790f0
flayner2 <flayner5@gmail.com>
2021-02-11 15:55:11 -0300

    Created a function to trim class 1 seqs

--- helpers/filter_seqs.py
+++ helpers/filter_seqs.py
@@ -1,3 +1,4 @@
+import re
 from Bio.Seq import Seq
 
 
@@ -5,9 +6,21 @@ def process_seq_by_class(seq: Seq, seq_class: int) -> Seq:
     if seq_class not in [1, 3, 6, 7]:
         return seq
 
+    if seq_class == 1:
+        return trim_x_only_keep_in_between(seq)
+
 
 def trim_x_only_keep_in_between(seq: Seq) -> Seq:
-    pass
+    pattern = re.compile(r"X+")
+    search_res = pattern.finditer(str(seq.seq))
+
+    x1 = next(search_res)
+    x2 = next(search_res)
+
+    _, x1_end = x1.span()
+    x2_start, _ = x2.span()
+
+    return seq[x1_end + 1 : x2_start]
 
 
 def trim_x_only(seq: Seq) -> Seq:
